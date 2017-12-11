019fdac60a7920ec39765db81c5e951144345f43
harbor <hluo@bdna.com>
2017-12-11 10:55:29 +0800

    exclude other APIs

--- cloud-mail/src/main/java/com/flexera/cloud/mail/CloudMaliApplication.java
+++ cloud-mail/src/main/java/com/flexera/cloud/mail/CloudMaliApplication.java
@@ -5,7 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -16,9 +23,27 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //@SpringBootApplication
 @SpringCloudApplication
 @EnableSwagger2
-@ComponentScan({"com.flexera.cloud.mail.controller"})
+@ComponentScan({"com.flexera.cloud.mail"})
 public class CloudMaliApplication {
+
     public static void main(String[] args){
         SpringApplication.run(CloudMaliApplication.class, args);
     }
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("com.flexera.cloud.mail.controller")
+                ).paths(PathSelectors.any()).build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("mail Service")
+                .description("Provide mail service")
+                .version("1.0.0")
+                .build();
+    }
+
 }
